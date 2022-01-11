e251e879ea566f6ab8d45b106ce85684256c9feb
saad-ken <siddiquisaad973@gmail.com>
2022-01-11 13:01:26 +0530

    Added search and used init tv maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
-  return <MainPageLayout>Home</MainPageLayout>;
+  const [input, setInput] = useState("");
+
+  const onInputChange = (ev) => {
+    setInput(ev.target.value);
+  };
+
+  const OnSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then((res) =>
+      res.json().then((result) => {
+        console.log(result);
+      })
+    );
+  };
+
+  const onKeydown = (ev) => {
+    if (ev.keyCode === 13) {
+      OnSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeydown}
+      />
+      <button type="button" onClick={OnSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
